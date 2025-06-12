6223fbbdc321dd97366eec3741287082442be231
Sumaiyya Patel <smahinn1@gmail.com>
2025-06-12 18:57:16 +0530

    v 3

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 // vite.config.js
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/sumaiyyapatel.github.io/', // important: must match your repo name
+  base: '/', // because you're using a username.github.io repo
+  plugins: [react()]
 });
